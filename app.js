df451fb7eb7a39bff8dc5f00b00d0dd09e4fe102
alex-arango47 <alexis.arango21@gmail.com>
2022-06-15 21:16:33 -0300

    actualizando

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
